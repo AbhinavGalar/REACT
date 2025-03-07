436c9cb243421119cfe23c4fe580a406b528b8cf
AbhinavGalar <abhinavgalar442@gmail.com>
2025-03-07 22:06:42 +0530

    react

--- API/src/App.jsx
+++ API/src/App.jsx
@@ -1,13 +1,15 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 function App() {
+  let [apidata, setApi]=useState([])
 useEffect (()=>{
-  axios.get('https://jsonplaceholder.typicode.com/comments')
+  axios.get('http://localhost:3000/student')
     .then(res=>{console.log(res.data);
-
+      setApi(res.data)
+   
+   
     })
-  
-
+    
 })
 
 
@@ -16,9 +18,17 @@ useEffect (()=>{
 
   return (
     <>
-      
+   {
+    apidata.map((e)=>{return <h1>{e.name} {e.age}</h1>})            
+   }
+                                            
     </>
   )
 }
 
 export default App
+// npm i react-icons
+// react icons  
+// npm i json-server
+//  
+
