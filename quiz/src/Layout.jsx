bc2cf222349a752adffba02c7fcabda4818b637d
AbhinavGalar <abhinavgalar442@gmail.com>
2025-03-17 23:45:37 +0530

    project

--- quiz/src/Layout.jsx
+++ quiz/src/Layout.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import "https://unpkg.com/aos@2.3.1/dist/aos.css" ;
+
 function Layout() {
   return (
     <>
@@ -58,60 +58,155 @@ function Layout() {
       {/* ==hero section============== */}
       <section id="hero">
         <article>
-            <div>
-                <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c58ca09cfed2280f16c31d_Asset%20Right.svg"></img>
-            </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c58ca09cfed2280f16c31d_Asset%20Right.svg"></img>
+          </div>
         </article>
         <article class="texts">
-       <div class="h1">
-        <h1>Introducing Instructional suite </h1>
-       </div>
+          <div class="h1">
+            <h1>Introducing Instructional suite </h1>
+          </div>
 
-       <div class="h2">
-<h1>"I had no idea quizizz</h1>
-       </div>
+          <div class="h2">
+            <h1>"I had no idea quizizz</h1>
+          </div>
 
-       <div class="h3">
-        <h1>could do that"</h1>
-       </div>
-<div class="h4">
-    <h1>- Almost everybody</h1>
-</div>
-<hr class="line"></hr>
+          <div class="h3">
+            <h1>could do that"</h1>
+          </div>
+          <div class="h4">
+            <h1>- Almost everybody</h1>
+          </div>
+          <hr class="line"></hr>
 
-   <div class="h4">
-    <h1>Create and deliver bell-to-bell curriculum<br></br>
-    resources that meet the needs of every student.</h1>
-   </div>
+          <div class="h4">
+            <h1>
+              Create and deliver bell-to-bell curriculum<br></br>
+              resources that meet the needs of every student.
+            </h1>
+          </div>
         </article>
 
         <article class="button">
-        <div class="btn1">teachers<br></br>
-        Sign Up For Free 
-        </div>
-<div class="btn2">
-  Admins <br></br>
-  Learn More 
-</div>
+          <div class="btn1">
+            teachers<br></br>
+            Sign Up For Free
+          </div>
+          <div class="btn2">
+            Admins <br></br>
+            Learn More
+          </div>
         </article>
 
         <article class="heroimg">
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e7b26e6c5e64cfe92_Logo%201.webp"></img></div>
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e682dcf3e2de72099_Logo%202.webp"></img></div>
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e7a159d9f46242e32_Logo%203.webp"></img></div>
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d2ca69d3901188708_Logo%204.webp"></img></div>
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d83705ec21d936d43_Logo%205.webp"></img></div>
-          <div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d2f95fc561a3ccd53_Logo%206.webp"></img></div>
-<div><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/676276645873b2dbb8eb7865_Instructure%20Research%20Team%20-%20ESSA%20Level%20IV%20Evidence%20-%202024%20-%202024-11-22.webp"></img></div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e7b26e6c5e64cfe92_Logo%201.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e682dcf3e2de72099_Logo%202.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5e7a159d9f46242e32_Logo%203.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d2ca69d3901188708_Logo%204.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d83705ec21d936d43_Logo%205.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/66c6ab5d2f95fc561a3ccd53_Logo%206.webp"></img>
+          </div>
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/676276645873b2dbb8eb7865_Instructure%20Research%20Team%20-%20ESSA%20Level%20IV%20Evidence%20-%202024%20-%202024-11-22.webp"></img>
+          </div>
         </article>
         <article class="texts1">
-<div class="t1">Trusted by teachers in 90% of U.S. Schools and 150+ countries.</div>
-<div class="t2">Now we support every part of your lesson.<br></br>
-Here’s how it works</div>
+          <div class="t1">
+            Trusted by teachers in 90% of U.S. Schools and 150+ countries.
+          </div>
+          <div class="t2">
+            Now we support every part of your lesson.<br></br>
+            Here’s how it works
+          </div>
         </article>
       </section>
-
+      <section id="footer">
+        <article class="f">
+          <div>
+            <img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/6792157ffa3e2e9204d4a3b6_%5Bcm%5D%20Brand.svg"></img>
+          </div>
+        </article>
+        <article class="f1">
+          <div>
+            <a href="The Quizz Blog">The Quizz Blog</a>
+          </div>
+          <div>
+            <a href="Teacher Resources">Teacher Resources</a>
+          </div>
+          <div>
+            <a href="Certified Educators">Certified Educators</a>
+          </div>
+          <div>
+            <a href="State Test Prep">State Test Prep</a>
+          </div>
+          <div>
+            <a href="Quizizz for Work">Quizizz for Work</a>
+          </div>
+          <div>
+            <a href="Help Center">Help Center</a>
+          </div>
+          <div>
+            <a href="Teacher Panel">Teacher Panel</a>
+          </div>
+          <div>
+            <a href="IQAPS">IQAPS</a>
+          </div>
+          <div>
+            <a href="AI Toolkit">AI Toolkit</a>
+          </div>
+          <div>
+            <a href="Accessibility and Inclusion">
+              Accessibility and Inclusion
+            </a>
+          </div>
+          <div>
+            <a href="Sitemap">Sitemap</a>
+          </div>
+          <div>
+            <a href="Terms of Service">Terms of Service</a>
+          </div>
+        </article>
+        <article class="f2">
+          <div>
+            <a href="Worksheets">Worksheets</a>
+          </div>
+          <div>
+            <a href="Reseller Program">Reseller Program</a>
+          </div>
+          <div>
+            <a href="Privacy Policy">Privacy Policy</a>
+          </div>
+          <div>
+            <a href="Privacy Center">Privacy Center</a>
+          </div>
+          <div>
+            <a href="Careers">Careers</a>
+          </div>
+          <div>
+            <a href="Contact Support">Contact Support</a>
+          </div>
+          <div>
+            <a href="About Us">About Us</a>
+          </div>
+        </article>
+        <article class="f3">
+          <div class="fi"><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/60aca2b71ab9a563ffecf204_google%20play.webp"></img></div>
+          <div class="fi1"><img src="https://cdn.prod.website-files.com/60aca2b71ab9a5e4ececf1cf/60aca2b71ab9a50fe9ecf205_apple%20app%20store.webp"></img></div>
+        </article>
+       
+      </section>
     </>
-  )
+  );
 }
 export default Layout;
